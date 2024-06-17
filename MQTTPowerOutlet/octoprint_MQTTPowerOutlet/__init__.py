38ff224d7e9a5328e0d20172ed3bc48ee1187fd4
rodgalv <164181363+rodgalv@users.noreply.github.com>
2024-06-17 11:10:54 +0200

    Add files via upload

---
+++ MQTTPowerOutlet/octoprint_MQTTPowerOutlet/__init__.py
@@ -0,0 +1,118 @@
+from __future__ import absolute_import
+
+import octoprint.plugin
+import flask
+import sys
+
+class MqttpoweroutletPlugin(octoprint.plugin.StartupPlugin,
+	octoprint.plugin.SettingsPlugin,
+	octoprint.plugin.AssetPlugin,
+	octoprint.plugin.TemplatePlugin,
+	octoprint.plugin.SimpleApiPlugin
+):
+
+	def get_settings_defaults(self):
+		return dict(
+				topics ="octoprint/plugin/poweroutlet/pub",
+				btn_1 = [dict(msgON = "ON", msgOFF = "OFF", state = False, icon="icon-home")],
+				btn_2 = [dict(msgON = "ON", msgOFF = "OFF", state = False, icon="icon-home")],
+				btn_3 = [dict(msgON = "ON", msgOFF = "OFF", state = False, icon="icon-home")],
+				btn_4 = [dict(msgON = "ON", msgOFF = "OFF", state = False, icon="icon-home")]
+		)
+
+	
+	def on_after_startup(self):
+		helpers = self._plugin_manager.get_helpers("mqtt", "mqtt_publish", "mqtt_subscribe", "mqtt_unsubscribe")
+		if helpers:
+			if "mqtt_publish" in helpers:
+				self.mqtt_publish = helpers["mqtt_publish"]
+			if "mqtt_subscribe" in helpers:
+				self.mqtt_subscribe = helpers["mqtt_subscribe"]
+			if "mqtt_unsubscribe" in helpers:
+				self.mqtt_unsubscribe = helpers["mqtt_unsubscribe"]
+			
+			try:			
+				self.mqtt_publish("octoprint/plugin/poweroutlet/pub", "MQTT TEST.")
+			except:
+				self._plugin_manager.send_plugin_message(self._identifier, dict(noMQTT=True))
+
+	def _on_mqtt_subscription(self, topic, message, retained=None, qos=None, *args, **kwargs):
+		self.mqtt_publish("octoprint/plugin/poweroutlet/pub", "echo: " + message)
+
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		# Define your plugin's asset files to automatically include in the
+		# core UI here.
+		return {
+			"js": ["js/MQTTPowerOutlet.js"],
+			"css": ["css/MQTTPowerOutlet.css"],
+			"less": ["less/MQTTPowerOutlet.less"]
+		}
+
+	def toggle(self, n):
+		btn_key = f"btn_{n}"
+		btn_settings = self._settings.get([btn_key])[0]
+		btn_settings["state"] = not btn_settings["state"]
+		self._settings.set([btn_key], [btn_settings])
+		self._settings.save()
+		self._logger.info("state: %s", btn_settings["state"])
+
+		if btn_settings["state"]:
+			self.mqtt_publish(self._settings.get(["topics"]), btn_settings["msgON"])
+		else: 
+			self.mqtt_publish(self._settings.get(["topics"]), btn_settings["msgOFF"])
+
+	##API
+	def get_api_commands(self):
+		return dict(toggle_btn=["n_btn"])
+
+	def on_api_command(self, command, data):
+		#import flask
+		
+		if command == "toggle_btn":
+			self._logger.info("Button toggled is {n_btn}".format(**data))
+			self.toggle(int("{n_btn}".format(**data)))
+
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		# Define the configuration for your plugin to use with the Software Update
+		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+		# for details.
+		return {
+			"MQTTPowerOutlet": {
+				"displayName": "Mqttpoweroutlet Plugin",
+				"displayVersion": self._plugin_version,
+
+				# version check: github repository
+				"type": "github_release",
+				"user": "rodgalv",
+				"repo": "MQTTPowerOutlet",
+				"current": self._plugin_version,
+
+				# update method: pip
+				"pip": "https://github.com/rodgalv/MQTTPowerOutlet/archive/{target_version}.zip",
+			}
+		}
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+__plugin_name__ = "MQTT Power Outlet"
+
+
+# Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
+# OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
+# OctoPrint 1.8.0 onwards only supports Python 3.
+__plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = MqttpoweroutletPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
---
+++ MQTTPowerOutlet/octoprint_MQTTPowerOutlet/static/js/MQTTPowerOutlet.js
@@ -0,0 +1,67 @@
+/*
+ * View model for MQTTPowerOutlet
+ *
+ * Author: Rodrigo Galvao Barros Silva
+ * License: AGPLv3
+ */
+$(function() {
+    function MqttpoweroutletViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        self.loginStateViewModel = parameters[0];
+        self.settingsViewModel = parameters[1];
+        
+        self.topics = ko.observable();
+        self.btn_1 = ko.observableArray();
+        self.btn_2 = ko.observableArray();
+        self.btn_3 = ko.observableArray();
+        self.btn_4 = ko.observableArray();
+       
+
+self.sendMsg = function(data) {
+    console.log(self.topics);
+    //console.log(self.btn_1);
+    console.log(self.settingsViewModel.settings.plugins.MQTTPowerOutlet.topics);
+    //alert(self.settingsViewModel.settings);
+    $.ajax({
+        url: API_BASEURL + "plugin/MQTTPowerOutlet",
+        type: "POST",
+        dataType: "json",
+        data: JSON.stringify({
+            command: "toggle_btn",
+            n_btn: data			
+        }),
+        contentType: "application/json; charset=UTF-8"
+    }).done(function(response){console.log(response);});
+}
+
+self.getAdditionalControls = function() {
+
+    var buttons = [
+        { name: "Power", type: "section", layout: "horizontal", children: [
+            {type: "javascript", javascript: function() { self.sendMsg(1); }, name: "Printer", enabled: "true"},
+            {type: "javascript", javascript: function() { self.sendMsg(2); }, name: "Fan", enabled: true},
+            {type: "javascript", javascript: function() { self.sendMsg(3); }, name: self.btn_1().icon, confirm: "*.", enabled: "true"},
+            {type: "javascript", javascript: function() { self.sendMsg(4); }, name: "*", confirm: "*.", enabled: "false"},
+
+        ]}
+    ];
+
+return buttons;
+};
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: MqttpoweroutletViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [  "loginStateViewModel", "settingsViewModel" ],
+        // Elements to bind to, e.g. #settings_plugin_MQTTPowerOutlet, #tab_plugin_MQTTPowerOutlet, ...
+        elements: [ "#settings_plugin_MQTTPowerOutlet"]
+    });
+});
